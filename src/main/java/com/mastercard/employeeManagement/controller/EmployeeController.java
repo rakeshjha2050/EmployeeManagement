00fe679123a3aadb43ed6f25bb47af0361c4f8a8
rakeshjha2050 <50776225+rakeshjha2050@users.noreply.github.com>
2021-07-06 21:11:00 +0530

    first commit

---
+++ src/main/java/com/mastercard/employeeManagement/controller/EmployeeController.java
@@ -0,0 +1,77 @@
+package com.mastercard.employeeManagement.controller;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.mastercard.employeeManagement.model.Employee;
+
+/**
+ * Handles requests for the Employee service.
+ */
+@Controller
+@RequestMapping("/homeController")
+public class EmployeeController {
+	
+	private static final Logger logger = LoggerFactory.getLogger(EmployeeController.class);
+	
+	//Map to store employees, ideally we should use database
+	Map<Integer, Employee> empData = new HashMap<Integer, Employee>();
+	
+	@RequestMapping(value = "/rest/emp/dummy", method = RequestMethod.GET)
+	public @ResponseBody Employee getDummyEmployee() {
+		logger.info("Start getDummyEmployee");
+		Employee emp = new Employee();
+		emp.setId(9999);
+		emp.setName("Dummy");
+		emp.setCreatedDate(new Date());
+		empData.put(9999, emp);
+		return emp;
+	}
+	
+	@RequestMapping(value = "/rest/emp/{id}", method = RequestMethod.GET)
+	public @ResponseBody Employee getEmployee(@PathVariable("id") int empId) {
+		logger.info("Start getEmployee. ID="+empId);
+		
+		return empData.get(empId);
+	}
+	
+	@RequestMapping(value = "/rest/emps", method = RequestMethod.GET)
+	public @ResponseBody List<Employee> getAllEmployees() {
+		logger.info("Start getAllEmployees.");
+		List<Employee> emps = new ArrayList<Employee>();
+		Set<Integer> empIdKeys = empData.keySet();
+		for(Integer i : empIdKeys){
+			emps.add(empData.get(i));
+		}
+		return emps;
+	}
+	
+	@RequestMapping(value = "/rest/emp/create", method = RequestMethod.POST)
+	public @ResponseBody Employee createEmployee(@RequestBody Employee emp) {
+		logger.info("Start createEmployee.");
+		emp.setCreatedDate(new Date());
+		empData.put(emp.getId(), emp);
+		return emp;
+	}
+	
+	@RequestMapping(value = "/rest/emp/delete/{id}", method = RequestMethod.PUT)
+	public @ResponseBody Employee deleteEmployee(@PathVariable("id") int empId) {
+		logger.info("Start deleteEmployee.");
+		Employee emp = empData.get(empId);
+		empData.remove(empId);
+		return emp;
+	}
+	
+}
